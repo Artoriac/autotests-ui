8d78736ff8f2f30b08e22c5f9111156f19a34787
Artoriac <yarrrr42@gmail.com>
2025-08-02 19:17:14 +0300

    fixes for 7.1 POM part2

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -5,6 +5,7 @@ from pages.base_page import BasePage
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
         self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
         self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
         self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
